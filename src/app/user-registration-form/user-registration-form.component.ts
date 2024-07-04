5ef7382b64ea9a1fbf640c5a01b860096e9f4184
Henry Nguyen <belugas2005@gmail.com>
2024-07-04 12:59:46 -0500

    Create user registration form

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+import { UserRegistrationService } from "../fetch-api-data.service";
+
+// Decorator tells Angular that this is a component
+@Component({
+  // Selector defines HTML element that component will render in
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrl: './user-registration-form.component.scss'
+})
+export class UserRegistrationFormComponent {
+
+  @Input() userData = {
+    Username: '',
+    Password: '',
+    Email: '',
+    Birthday: ''
+  };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+ngOnInit(): void {
+}
+
+// Sending the form inputs to the backend
+registerUser(): void {
+  this.fetchApiData.userRegistration(this.userData)
+    .subscribe((result) => {
+      // Logic for a successful user registration goes here!
+      // Close modal on success
+      this.dialogRef.close();
+      console.log(result);
+      this.snackBar.open("User registered successfully!", "OK", {
+        duration: 2000,
+      });
+    }, (result) => {
+      this.snackBar.open(result, "OK", {
+        duration: 2000,
+      });
+    });
+  }
+}
