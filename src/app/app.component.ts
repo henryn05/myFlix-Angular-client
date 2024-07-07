bdf4fa968501a116a6a1fa5895d634eb95d02bef
Henry Nguyen <belugas2005@gmail.com>
2024-07-07 15:16:38 -0500

    Transfer code from app to welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -13,23 +8,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  // Opens dialog when signup button is clicked
-  constructor(public dialog: MatDialog) { }
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
-    });
-  }
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: "500px"
-    });
-  }
 }
